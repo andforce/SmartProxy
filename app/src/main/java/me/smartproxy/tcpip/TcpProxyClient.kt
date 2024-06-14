32d9fbf27a1f4de554b921094cb60c9a352fbd51
dy <86118@163.com>
2024-06-14 22:40:30 +0800

    refactor code

--- app/src/main/java/me/smartproxy/tcpip/TcpProxyClient.kt
+++ app/src/main/java/me/smartproxy/tcpip/TcpProxyClient.kt
@@ -25,89 +25,94 @@ class TcpProxyClient(pfd: ParcelFileDescriptor, buffer: ByteArray, private val v
 
 
     fun onTCPPacketReceived(ipHeader: IPHeader, size: Int) {
-        val tcpHeader = tcpHeader
-
         tcpHeader.offset = ipHeader.headerLength
+
         if (ipHeader.sourceIP == vpnLocalIpInt) {
             // 收到本地 TcpProxyServer 服务器数据
             if (tcpHeader.sourcePort == TcpProxyHelper.getPort()) {
-                val session =
-                    NatSessionManager.getSession(tcpHeader.destinationPort.toInt())
-                if (session != null) {
-                    Log.d(
-                        TAG,
-                        "onIPPacketReceived: 收到本地 TcpProxyServer 服务器数据, $ipHeader $tcpHeader"
-                    )
-                    ipHeader.sourceIP = ipHeader.destinationIP
-                    tcpHeader.sourcePort = session.remotePort
-                    ipHeader.destinationIP = vpnLocalIpInt
-
-                    CommonMethods.computeTCPChecksum(ipHeader, tcpHeader)
-                    vpnOutputStream?.write(ipHeader.data, ipHeader.offset, size)
-                    vpnOutputStream?.flush()
-                    receivedBytes += size.toLong()
-                } else {
-                    Log.d(
-                        TAG,
-                        "onIPPacketReceived: NoSession, $ipHeader $tcpHeader"
-                    )
-                }
+                natToRealClient(ipHeader, size)
             } else {
                 // 添加端口映射
-
-                val portKey = tcpHeader.sourcePort.toInt()
-                var session = NatSessionManager.getSession(portKey)
-                if (session == null || session.remoteIP != ipHeader.destinationIP || session.remotePort != tcpHeader.destinationPort) {
-                    session = NatSessionManager.createSession(
-                        portKey,
-                        ipHeader.destinationIP,
-                        tcpHeader.destinationPort
-                    )
-                }
-
-                session?.let {
-                    session.lastNanoTime = System.nanoTime()
-                    session.packetSent++ //注意顺序
-
-                    val tcpDataSize = ipHeader.dataLength - tcpHeader.headerLength
-                    if (session.packetSent == 2 && tcpDataSize == 0) {
-                        return  //丢弃tcp握手的第二个ACK报文。因为客户端发数据的时候也会带上ACK，这样可以在服务器Accept之前分析出HOST信息。
-                    }
-
-                    //分析数据，找到host
-                    if (session.bytesSent == 0 && tcpDataSize > 10) {
-                        val dataOffset = tcpHeader.offset + tcpHeader.headerLength
-                        val host = HttpHostHeaderParser.parseHost(
-                            tcpHeader.data,
-                            dataOffset,
-                            tcpDataSize
-                        )
-                        if (host != null) {
-                            session.remoteHost = host
-                        }
-                    }
-
-                    // 转发给本地 TcpProxyServer 服务器
-                    Log.d(
-                        TAG,
-                        "onIPPacketReceived: 转发给本地 TcpProxyServer 服务器, $ipHeader $tcpHeader"
-                    )
-                    ipHeader.sourceIP = ipHeader.destinationIP
-                    ipHeader.destinationIP = vpnLocalIpInt
-                    tcpHeader.destinationPort = TcpProxyHelper.getPort()
-
-                    CommonMethods.computeTCPChecksum(ipHeader, tcpHeader)
-                    vpnOutputStream?.write(ipHeader.data, ipHeader.offset, size)
-                    vpnOutputStream?.flush()
-                    session.bytesSent += tcpDataSize //注意顺序
-                    sentBytes += size.toLong()
-                }
+                natToTcpProxyServer(ipHeader, size)
             }
         } else {
             Log.e(TAG, "onIPPacketReceived, TCP: 收到非本地数据包, $ipHeader $tcpHeader")
         }
     }
 
+    private fun natToRealClient(ipHeader: IPHeader, size: Int) {
+        val session =
+            NatSessionManager.getSession(tcpHeader.destinationPort.toInt())
+        if (session != null) {
+            Log.d(
+                TAG,
+                "onIPPacketReceived: 收到本地 TcpProxyServer 服务器数据, $ipHeader $tcpHeader"
+            )
+            ipHeader.sourceIP = ipHeader.destinationIP
+            tcpHeader.sourcePort = session.remotePort
+            ipHeader.destinationIP = vpnLocalIpInt
+
+            CommonMethods.computeTCPChecksum(ipHeader, tcpHeader)
+            vpnOutputStream?.write(ipHeader.data, ipHeader.offset, size)
+            vpnOutputStream?.flush()
+            receivedBytes += size.toLong()
+        } else {
+            Log.d(
+                TAG,
+                "onIPPacketReceived: NoSession, $ipHeader $tcpHeader"
+            )
+        }
+    }
+
+    private fun natToTcpProxyServer(ipHeader: IPHeader, size: Int) {
+        val portKey = tcpHeader.sourcePort.toInt()
+        var session = NatSessionManager.getSession(portKey)
+        if (session == null || session.remoteIP != ipHeader.destinationIP || session.remotePort != tcpHeader.destinationPort) {
+            session = NatSessionManager.createSession(
+                portKey,
+                ipHeader.destinationIP,
+                tcpHeader.destinationPort
+            )
+        }
+
+        session?.let {
+            session.lastNanoTime = System.nanoTime()
+            session.packetSent++ //注意顺序
+
+            val tcpDataSize = ipHeader.dataLength - tcpHeader.headerLength
+            if (session.packetSent == 2 && tcpDataSize == 0) {
+                return  //丢弃tcp握手的第二个ACK报文。因为客户端发数据的时候也会带上ACK，这样可以在服务器Accept之前分析出HOST信息。
+            }
+
+            //分析数据，找到host
+            if (session.bytesSent == 0 && tcpDataSize > 10) {
+                val dataOffset = tcpHeader.offset + tcpHeader.headerLength
+                val host = HttpHostHeaderParser.parseHost(
+                    tcpHeader.data,
+                    dataOffset,
+                    tcpDataSize
+                )
+                if (host != null) {
+                    session.remoteHost = host
+                }
+            }
+
+            // 转发给本地 TcpProxyServer 服务器
+            Log.d(
+                TAG,
+                "onIPPacketReceived: 转发给本地 TcpProxyServer 服务器, $ipHeader $tcpHeader"
+            )
+            ipHeader.sourceIP = ipHeader.destinationIP
+            ipHeader.destinationIP = vpnLocalIpInt
+            tcpHeader.destinationPort = TcpProxyHelper.getPort()
+
+            CommonMethods.computeTCPChecksum(ipHeader, tcpHeader)
+            vpnOutputStream?.write(ipHeader.data, ipHeader.offset, size)
+            vpnOutputStream?.flush()
+            session.bytesSent += tcpDataSize //注意顺序
+            sentBytes += size.toLong()
+        }
+    }
 
     fun sendUDPPacket(ipHeader: IPHeader, udpHeader: UDPHeader?) {
         try {
