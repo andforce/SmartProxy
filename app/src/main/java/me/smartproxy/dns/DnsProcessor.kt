b9996f9bc3f3e11710760bb1a62bdeae1317a311
dy <86118@163.com>
2024-06-20 10:27:29 +0800

    添加Log

--- app/src/main/java/me/smartproxy/dns/DnsProcessor.kt
+++ app/src/main/java/me/smartproxy/dns/DnsProcessor.kt
@@ -12,13 +12,32 @@ class DnsProcessor(buffer: ByteArray, private val vpnLocalIpInt: Int) {
 
     fun processUdpPacket(header: IPHeader, dnsProxy: DnsProxy?) {
         udpHeader.offset = header.headerLength
+
+        Logger.e(
+            TAG,
+            "processUdpPacket, UDP: 收到数据包, $header $udpHeader")
+
         if (header.sourceIP == vpnLocalIpInt && udpHeader.destinationPort.toInt() == 53) {
             dnsBuffer.clear()
             dnsBuffer.limit(header.dataLength - 8)
             val dnsPacket = DnsPacket.takeFromPoll(dnsBuffer)
+            Logger.e(
+                TAG,
+                "processUdpPacket, UDP: 收到DNS数据包, $dnsPacket"
+            )
             dnsPacket?.let {
                 if (dnsPacket.dnsHeader.questionCount > 0) {
+                    Logger.e(
+                        TAG,
+                        "processUdpPacket, UDP: DNS数据包无问题, onDnsRequestReceived(), questionCount is ${dnsPacket.dnsHeader.questionCount}"
+                    )
+
                     dnsProxy?.onDnsRequestReceived(header, udpHeader, dnsPacket)
+                } else {
+                    Logger.e(
+                        TAG,
+                        "processUdpPacket, UDP: DNS数据包无问题, questionCount is 0"
+                    )
                 }
             }
         } else {
